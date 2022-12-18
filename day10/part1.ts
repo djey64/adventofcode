68e2ba18c69839a8d24b2fcff82960beaeee4f02
djey64 <djey64@gmail.com>
2022-12-19 00:09:38 +0100

    Day10 finished

--- day10/part1.ts
+++ day10/part1.ts
@@ -6,3 +6,37 @@ const input = fs
   .toString()
   .trim()
   .split("\n");
+
+const cycles: Array<() => void> = [];
+let x = 1;
+let signals = 0;
+
+input.forEach((str) => {
+  if (str.startsWith("noop")) {
+    cycles.push(() => {});
+  } else {
+    const [_, amount] = str.split(" ");
+    cycles.push(() => {});
+    cycles.push(() => {
+      x += parseInt(amount);
+    });
+  }
+});
+
+let cycle = 1;
+while (cycles.length > 0) {
+  if (
+    cycle === 20 ||
+    cycle === 60 ||
+    cycle === 100 ||
+    cycle === 140 ||
+    cycle === 180 ||
+    cycle === 220
+  ) {
+    signals += cycle * x;
+  }
+  cycles.shift().call("");
+
+  cycle++;
+}
+console.log(signals);
--- day10/part2.ts
+++ day10/part2.ts
@@ -6,3 +6,33 @@ const input = fs
   .toString()
   .trim()
   .split("\n");
+
+const cycles: Array<() => void> = [];
+let x = 1;
+
+input.forEach((str) => {
+  if (str.startsWith("noop")) {
+    cycles.push(() => {});
+  } else {
+    const [_, amount] = str.split(" ");
+    cycles.push(() => {});
+    cycles.push(() => {
+      x += parseInt(amount);
+    });
+  }
+});
+
+let cycle = 0;
+let CRT = "";
+const screen: string[] = [];
+while (cycles.length > 0) {
+  if (cycle !== 0 && cycle % 40 === 0) {
+    screen.push(CRT);
+    CRT = "";
+  }
+  CRT += cycle % 40 >= x - 1 && cycle % 40 <= x + 1 ? "#" : ".";
+  cycles.shift().call("");
+  cycle++;
+}
+screen.push(CRT);
+console.log(screen);
