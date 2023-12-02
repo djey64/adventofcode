e78728646d7f67945b987ea6055e57bc925dfc35
Djey Djey <djey64@gmail.com>
2023-12-03 00:45:08 +0100

    Prepare Day3

---
+++ 2023/day3/part1.ts
@@ -0,0 +1,11 @@
+import path from "path";
+import fs from "fs";
+
+export const input = fs
+    .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+    .toString()
+    .trim()
+    .split("\n");
+
+input.forEach(line => {
+})
