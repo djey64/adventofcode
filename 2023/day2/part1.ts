d84acb3fdd7086cd6cff0c6272b44e19c92a228a
Djey Djey <djey64@gmail.com>
2023-12-03 00:42:19 +0100

    Day 2

--- 2023/day2/part1.ts
+++ 2023/day2/part1.ts
@@ -1,10 +1,34 @@
 import path from "path";
 import fs from "fs";
 
-const __dirname = new URL('.', import.meta.url).pathname.slice(1);
 export const input = fs
     .readFileSync(path.join(__dirname, "input.txt"), "utf8")
     .toString()
     .trim()
     .split("\n");
-   
+
+const MAX: {[key: string]: number} = {
+    red: 12,
+    green: 13,
+    blue: 14
+}
+
+let count = 0
+input.forEach(line => {
+    const [_, gameIndex] = line.match('Game (\\d+)')
+    const sets = line.split(';')
+    let impossible = false
+    sets.forEach(set => {
+        const cubes = set.split(',')
+        cubes.forEach(cube => {
+            const [_, n, color] = cube.match('(\\d+) (blue|green|red)')
+            if(parseInt(n) > MAX[color]) {
+                impossible = true
+                return
+            }
+        })
+        if(impossible) return
+    })
+    if(!impossible) count += parseInt(gameIndex)
+})
+console.log(count)
--- 2023/day2/part2.ts
+++ 2023/day2/part2.ts
@@ -1,9 +1,23 @@
 import path from "path";
 import fs from "fs";
 
-const __dirname = new URL('.', import.meta.url).pathname.slice(1);
 export const input = fs
-    .readFileSync(path.join(__dirname, "input2.txt"), "utf8")
+    .readFileSync(path.join(__dirname, "input.txt"), "utf8")
     .toString()
     .trim()
     .split("\n");
+
+let count = 0
+input.forEach(line => {
+    const sets = line.split(';')
+    const fewest: {[key: string]: number} = {red: 0, green: 0, blue: 0}
+    sets.forEach(set => {
+        const cubes = set.split(',')
+        cubes.forEach(cube => {
+            const [_, n, color] = cube.match('(\\d+) (blue|green|red)')
+            fewest[color] = Math.max(parseInt(n), fewest[color])
+        })
+    })
+    count += fewest.red * fewest.green * fewest.blue
+})
+console.log(count)
