6929da35ffe71524854e8710c7e6e4d37db92bfc
Djey Djey <djey64@gmail.com>
2023-12-03 15:25:49 +0100

    part2

--- 2023/day3/part2.ts
+++ 2023/day3/part2.ts
@@ -8,5 +8,47 @@ export const input = fs
     .trim()
     .split("\n");
 
-input.forEach(line => {
+const getAdjNumbers = (r: RegExpMatchArray[], x: number): number[] => {
+    const numbers: number[] = []
+    r.forEach(match => {
+        const [ number ] = match
+        const { index: start } = match
+        const end = start + number.length - 1
+        if(Math.abs(x - start) <= 1 || Math.abs(x - end) <= 1 || (start < x && end > x)) {
+            numbers.push(parseInt(number))
+        }
+    })
+    return numbers
+}
+
+const getGearRatio = (x: number, y: number, input: string[]): number => {
+    // Check line above
+    let count = 0
+    const maxX = input[0].length - 1
+    const maxY = input.length - 1
+    const numbers: number[] = []
+    if(y > 0) {
+        const r = [...input[y - 1].matchAll(/\d+/g)]
+        numbers.push(...getAdjNumbers(r, x))
+    }
+    // Check current line
+    const r = [...input[y].matchAll(/\d+/g)]
+    numbers.push(...getAdjNumbers(r, x))
+    // Check line below
+    if(y < maxY) {
+        const r = [...input[y + 1].matchAll(/\d+/g)]
+        numbers.push(...getAdjNumbers(r, x))
+    }
+
+    return numbers.length == 2 ? numbers[0] * numbers[1] : 0
+}
+
+let ratio = 0
+input.forEach((line, y) => {
+    const r = [...line.matchAll(/\*/g)]
+    r.forEach(match => {
+        const { index: x } = match
+        ratio += getGearRatio(x, y, input)
+    })
 })
+console.log(ratio)
