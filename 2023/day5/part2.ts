b8cca46217d02523d4a606a51f26bfec985abd7a
petitjej <jeremy.petit-jean.external@ifpen.fr>
2023-12-13 15:19:18 +0100

    prepare day 5

---
+++ 2023/day5/part2.ts
@@ -0,0 +1,11 @@
+import path from "path";
+import fs from "fs";
+
+// https://adventofcode.com/2023/day/5
+export const input = fs
+    .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+    .toString()
+    .trim()
+    .split("\n");
+
+// TODO
