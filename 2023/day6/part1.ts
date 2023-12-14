295479ef10d3fb46006bec93a1f28e0d803638fc
petitjej <jeremy.petit-jean.external@ifpen.fr>
2023-12-14 17:07:17 +0100

    parsing

--- 2023/day6/part1.ts
+++ 2023/day6/part1.ts
@@ -3,7 +3,12 @@ import fs from "fs";
 
 // https://adventofcode.com/2023/day/6
 export let input = fs
-    .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+    .readFileSync(path.join(__dirname, "exemple.txt"), "utf8")
     .toString()
     .trim()
     .split("\n");
+
+/* Parsing */
+const [timeLine, distanceLine] = input
+const time = timeLine.match(/\d+/g).map(d => parseInt(d))
+const distance = distanceLine.match(/\d+/g).map(d => parseInt(d))
