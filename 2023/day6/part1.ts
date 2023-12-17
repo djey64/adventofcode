68ea01dabfcdc886ecd97e19b40f3dbfe80c6ec3
Djey Djey <djey64@gmail.com>
2023-12-17 11:44:30 +0100

    day 6

--- 2023/day6/part1.ts
+++ 2023/day6/part1.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 
 // https://adventofcode.com/2023/day/6
 export let input = fs
-    .readFileSync(path.join(__dirname, "exemple.txt"), "utf8")
+    .readFileSync(path.join(__dirname, "input.txt"), "utf8")
     .toString()
     .trim()
     .split("\n");
@@ -12,3 +12,16 @@ export let input = fs
 const [timeLine, distanceLine] = input
 const time = timeLine.match(/\d+/g).map(d => parseInt(d))
 const distance = distanceLine.match(/\d+/g).map(d => parseInt(d))
+
+const result = time.map((b, i) => {
+    const a = -1
+    const c = distance[i]
+    // Formule quadratique (-b +- √(b^2 - 4ac)) / 2a
+    let x1 = (-b + Math.sqrt(b * b - 4 * a * (-c))) / 2 * a
+    let x2 = (-b - Math.sqrt(b * b - 4 * a * (-c))) / 2 * a
+    if(x1 % 1 == 0) x1 += 1
+    if(x2 % 1 == 0) x2 -= 1
+    return Math.floor(x2) - Math.ceil(x1) + 1
+}).reduce((acc, n) => acc * n)
+
+console.log(result)
--- 2023/day6/part2.ts
+++ 2023/day6/part2.ts
@@ -7,3 +7,20 @@ export let input = fs
     .toString()
     .trim()
     .split("\n");
+
+/* Parsing */
+const [timeLine, distanceLine] = input
+const time = timeLine.match(/\d+/g).reduce((acc, s) => acc+s)
+const distance = distanceLine.match(/\d+/g).reduce((acc, s) => acc+s)
+
+const b = parseInt(time)
+const c = parseInt(distance)
+const a = -1
+
+// Formule quadratique (-b +- √(b^2 - 4ac)) / 2a
+let x1 = (-b + Math.sqrt(b * b - 4 * a * (-c))) / 2 * a
+let x2 = (-b - Math.sqrt(b * b - 4 * a * (-c))) / 2 * a
+if(x1 % 1 == 0) x1 += 1
+if(x2 % 1 == 0) x2 -= 1
+
+console.log(Math.floor(x2) - Math.ceil(x1) + 1)
